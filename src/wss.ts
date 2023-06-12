3cca33f3d19152f566f797c0c81d984b92d6f61e
jackhuang <2504448153@qq.com>
2023-06-12 11:41:03 +0800

    test: actions
    Signed-off-by: jackhuang <2504448153@qq.com>

--- src/wss.ts
+++ src/wss.ts
@@ -1,22 +1,27 @@
 import path from 'path';
 import { execa } from 'execa';
 import fs from 'fs-extra';
+import stream from 'stream';
 // import { fileURLToPath } from 'url';
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
-const reg = /公共链接：(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*))/;
+const reg = /公共链接：(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*))/m;
 
 export async function uploadWss(filePath: string) {
-    const logPath = path.resolve(__dirname, './log', `${+new Date()}`);
-    await fs.ensureFile(logPath)
     let lp = execa(`python3`, [`${path.resolve(__dirname, './python/wss.py')}`, `upload`, `${filePath}`], {
-        stdout: process.stdout,
         stderr: process.stderr,
     });
-    lp.pipeStdout?.(fs.createWriteStream(logPath, 'utf-8'));
+    let wt = new stream.Writable();
+    const chunks: Buffer[] = [];
+    wt._write = (chunk, encoding, done) => {
+        chunks.push(Buffer.from(chunk))
+        console.log(chunk.toString('utf8'));
+        done();
+    }
+    lp.pipeStdout?.(wt);
     await lp;
-    let out = await fs.readFile(logPath, 'utf-8')
+    let out = Buffer.concat(chunks).toString('utf8');
     let link = reg.exec(out)?.[1];
     console.log('wss out: ', out);
     console.log('wss link: ', link);
